a8f5adeae6efe8e434632dbe19d1358a8453e607
magicalcyber <magicalcyber@10.10.10.12>
2017-10-30 02:05:48 +0700

    Init

---
+++ src/main/java/com/magicalcyber/investmentstock/InvestmentStockApplication.java
@@ -0,0 +1,12 @@
+package com.magicalcyber.investmentstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentStockApplication.class, args);
+	}
+}
